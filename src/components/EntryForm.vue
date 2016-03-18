5ca97a9e16a365a2cbd29b25d06feba9c71a5369
Connor Brathwaite <connor.brathwaite@gmail.com>
2016-03-18 16:48:53 -0400

    :massage: population of select options

--- src/components/EntryForm.vue
+++ src/components/EntryForm.vue
@@ -32,7 +32,7 @@ input[type=number]::-webkit-outer-spin-button {
           <select class="browser-default" v-model="entry.project" :value="project">
             <option value="" disabled selected>Project</option>
             <option v-for="project in projects">
-              {{ project }}
+              {{ project.name }}
             </option>
           </select>
         </div>
@@ -71,50 +71,27 @@ export default {
   data () {
     return {
       entry: { member: {name: '', avatar: ''}, project: '', hours: '', notes: '', created: '' },
-      projects: ['tickbot', 'tickbot-server', 'tickbot-frontend'],
-      members: [
-        {
-          id: 'U034QMY7N',
-          team_id: 'T034QMY7L',
-          name: 'alex',
-          deleted: false,
-          status: null,
-          color: '9f69e7',
-          real_name: 'Alexander Haniotis',
-          tz: 'America/Los_Angeles',
-          tz_label: 'Pacific Daylight Time',
-          tz_offset: -25200,
-          profile: {
-            image_24: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_24.jpg',
-            image_32: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_32.jpg',
-            image_48: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_48.jpg',
-            image_72: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_72.jpg',
-            image_192: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_192.jpg',
-            image_original: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_original.jpg',
-            first_name: 'Alexander',
-            last_name: 'Haniotis',
-            title: 'Partner, Design',
-            skype: '',
-            phone: '514-926-8972',
-            image_512: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_192.jpg',
-            image_1024: 'https://avatars.slack-edge.com/2015-10-07/12106965937_eb3eedd4e63414f512cf_192.jpg',
-            real_name: 'Alexander Haniotis',
-            real_name_normalized: 'Alexander Haniotis',
-            email: 'alex@volume7.io'
-          },
-          is_admin: true,
-          is_owner: true,
-          is_primary_owner: true,
-          is_restricted: false,
-          is_ultra_restricted: false,
-          is_bot: false
-        }
-      ]
+      projects: [],
+      members: []
     }
+  },
+  ready () {
+    this.getUsers()
+    this.getProjects()
   },
   methods: {
     newEntry () {
       socket.emit('post', { entry: this.entry })
+    },
+    getUsers () {
+      this.$http.get('https://slack.com/api/users.list?token=xoxp-3160746258-17429586099-26955572661-b1293c7688&pretty=1')
+        .then(data => this.$set('members', data.data.members))
+        .catch(e => console.log(e))
+    },
+    getProjects () {
+      this.$http.get('https://slack.com/api/channels.list?token=xoxp-3160746258-17429586099-26955572661-b1293c7688&pretty=1')
+        .then(data => this.$set('projects', data.data.channels))
+        .catch(e => console.log(e))
     }
   }
 }
